abb5ddc113a735386b93d2214234f536d3e4e2de
quinntyx <harrisonhenry916@gmail.com>
2026-02-25 12:33:21 -0600

    initial commit

---
+++ src/embedding.rs
@@ -0,0 +1,13 @@
+
+
+fn embed(s: &[String]) -> Result<Vec<Embeddings, Error>> {
+    let mut home: PathBuf = env::current_dir().unwrap();
+    home.push("path-to-model");
+
+    let sbert_model = SBertHF::new(home.to_str().unwrap());
+
+    let batch_size = 64;
+
+    sbert_model.forward(texts.to_vec(), batch_size)
+}
+
