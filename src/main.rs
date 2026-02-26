e7baa286be6f9a3f8dbcdeb9aa18fea748dec1f3
miloa <crabapplefarms@icloud.com>
2026-02-26 12:37:32 -0600

    testing stuffs

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@ pub mod embedding;
 pub mod generate;
 
 fn main() {
-    let v = vec!["test", "test"];
+    let v = vec!["test", "test2"];
+    let vs: Vec<String> = v.iter().map(|s| s.to_string()).collect();
+    let _ = embedding::embed(&vs);
 }
