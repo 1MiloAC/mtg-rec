120b43a959b90d53434af54dc4c1533e9ea179c5
miloa <crabapplefarms@icloud.com>
2026-03-03 11:09:52 -0600

    schema

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,16 @@
-use arrow_array::{RecordBatch,RecordBatchIterator}
-use arrow_schema::{DataType, Field, Schema}
+use arrow_array::{RecordBatch, RecordBatchIterator};
+use arrow_schema::{DataType, Field, Schema};
 use sbert::{Embeddings, Error, SBertHF};
+use std::sync::Arc;
 
-pub async fn data(embeds: Vec::<Embeddings>){
+pub async fn data(embeds: Vec<Embeddings>) {
     let db = lancedb::connect("../db/").execute().await.unwrap();
+    let schema = Arc::new(Schema::new(vec![
+        Field::new("id", DataType::Int32, false),
+        Field::new(
+            "vector",
+            DataType::FixedSizeList(Arc::new(Field::new("item", DataType::Float32, true)), 128),
+            true,
+        ),
+    ]));
 }
