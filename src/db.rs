def1f505e5c49d59b0a0406cb95d0bbb7205361a
miloa <crabapplefarms@icloud.com>
2026-03-04 15:36:09 -0600

    Swapped to onnx runtime and connected embedding to the db. Also wrapped the runtime in tokio s othat async block_on could be run on the database creation function

--- src/db.rs
+++ src/db.rs
@@ -1,15 +1,15 @@
 use arrow_array::{
-    FixedSizeListArray, Int32Array, ListArray, RecordBatch, RecordBatchIterator, types::Float32Type,
+    FixedSizeListArray, ListArray, RecordBatch, RecordBatchIterator, types::Float32Type,
 };
 use arrow_schema::{DataType, Field, Schema};
-use sbert::Embeddings;
+use fastembed::Embedding;
 use std::sync::Arc;
 
-pub async fn data(embeds: Vec<Embeddings>) {
-    let db = lancedb::connect("../db/").execute().await.unwrap();
+pub async fn data(embeds: Vec<Embedding>) {
+    let db = lancedb::connect("./db").execute().await.unwrap();
     let schema = Arc::new(Schema::new(vec![Field::new(
         "embedding",
-        DataType::FixedSizeList(Arc::new(Field::new("item", DataType::Float32, false)), 128),
+        DataType::FixedSizeList(Arc::new(Field::new("item", DataType::Float32, true)), 768),
         true,
     )]));
     let data = FixedSizeListArray::from_iter_primitive::<Float32Type, _, _>(
@@ -17,8 +17,9 @@ pub async fn data(embeds: Vec<Embeddings>) {
             let v = s.to_vec();
             Some(v.into_iter().map(Some))
         }),
-        128,
+        768,
     );
+    println!("ingest successful");
     let batch = RecordBatch::try_new(schema.clone(), vec![Arc::new(data)]).unwrap();
     let batch_i = RecordBatchIterator::new(vec![batch].into_iter().map(Ok), schema.clone());
     db.create_table("test", Box::new(batch_i))
--- src/embedding.rs
+++ src/embedding.rs
@@ -1,11 +1,9 @@
 use fastembed::{Embedding, EmbeddingModel, Error, InitOptions, TextEmbedding};
 use std::{env, path::PathBuf};
 
-pub fn data(data: &[String]) -> Result<Vec<Embedding>, Error> {
-    let mut model = TextEmbedding::try_new(Default::default())?;
-
+pub fn embd(data: &[String]) -> Result<Vec<Embedding>, Error> {
     let mut model = TextEmbedding::try_new(
         InitOptions::new(EmbeddingModel::NomicEmbedTextV15).with_show_download_progress(true),
     )?;
-    model?.embed(data, None).unwrap()
+    model.embed(data, None)
 }
